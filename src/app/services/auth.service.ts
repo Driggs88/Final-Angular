9f1e779eb9fb6f3d08c00b82003b2c184a2f03ff
Driggs88 <riggs_david@hotmail.com>
2018-01-09 23:32:08 -0500

    finished auth services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,5 +26,67 @@ export class AuthService {
       { withCredentials: true }
     )
 
+    // Convert from observable to promise
+    .toPromise()
+
+    // Parse the JSON
+    .then(res => res.json());
   }
+
+  login(componentInfo){
+    return this.httpTool
+    .post(
+      `${environment.apiBase}/api/login`,
+
+      {
+        enterEmail: componentInfo.email,
+        enterPassword: componentInfo.password
+      },
+
+      // Send the cookies across domains
+      { withCredentials: true }
+    )
+
+    //Convert from observable to promise
+    .toPromise()
+
+    // Parse the JSON
+    .then(res => res.json());
+  }
+
+  logout() {
+    return this.httpTool
+      .post(
+        `${environment.apiBase}/api/logout`,
+
+        // Nothing to send to the back end
+        {},
+
+        // send the cookies across domains
+        { withCredentials: true }
+      )
+
+      // Convert from observable to promise
+      .toPromise()
+
+      // Parse the JSON
+      .then(res => res.json());
+  }
+
+  checklogin() {
+      return this.httpTool
+      .get(
+        `${environment.apiBase}/api/checklogin`,
+
+        // Send the cookies across domains
+        { withCredentials: true }
+      )
+
+      // Convert from observable to promise
+      .toPromise()
+
+      // Parse the JSON
+      .then(res => res.json());
+  }
+
 }
