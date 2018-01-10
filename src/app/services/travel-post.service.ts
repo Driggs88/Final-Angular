84a6aec32f17737b68b6cf4cb758ea8c80b2a137
Driggs88 <riggs_david@hotmail.com>
2018-01-10 00:39:43 -0500

    Finished travel post services

--- src/app/services/travel-post.service.ts
+++ src/app/services/travel-post.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class TravelPostService {
 
-  constructor() { }
+  constructor(
+    private httpTool: Http
+  ) { }
+
+  newTravelPost(componentInfo) {
+    return this.httpTool
+    .post(
+      `${environment.apiBase}/api/travelPost`,
+
+      // Form body Info to send to the back end
+      componentInfo,
+
+      // Send the cookies across domains
+      { withCredentials: true }
+    )
+
+    // Parse the JSON
+    .map(res => res.json());
+  }
+
+  allTravelPost() {
+    return this.httpTool
+    .get(
+      `${environment.apiBase}/api/travelPost`,
+
+      // Send the cookies across domains
+      { withCredentials: true }
+    )
+
+    // Parse the JSON
+    .map(res => res.json());
+  }
 
 }
